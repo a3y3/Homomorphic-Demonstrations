08480aba7ee20618d8abfcb351262e494be1a8aa
a3y3 <sohamssd@gmail.com>
2020-10-13 12:51:42 -0400

    Finish Matrix Multiplication

--- source/MatrixMultiplier.cpp
+++ source/MatrixMultiplier.cpp
@@ -4,9 +4,90 @@
 
 #include <iostream>
 #include "MatrixMultiplier.h"
+#include "Encryptor.h"
+#include "Util.h"
+#include "DotProduct.h"
 
 void MatrixMultiplier::main() {
+    int plaintext_prime_modulus = 53;
+    int phiM = 3000;
+    int lifting = 1;
+    int numOfBitsOfModulusChain = 512;
+    int numOfColOfKeySwitchingMatrix = 2;
 
+    COED::Util::info("Starting program ...");
+
+    COED::Encryptor encryptor("/tmp/sk.txt", "/tmp/pk.txt",
+                              plaintext_prime_modulus,
+                              phiM,
+                              lifting,
+                              numOfBitsOfModulusChain,
+                              numOfColOfKeySwitchingMatrix);
+
+    int a[NCOLS][NROWS];
+    int b[NCOLS][NROWS];
+    MatrixMultiplier::accept_inputs(a, b);
+    COED::Util::info("Matrix A:");
+    display_matrix(a);
+    COED::Util::info("Matrix B:");
+    display_matrix(b);
+
+    /* Transposing the second matrix allows us to effectively represent matrix multiplication as M*N dot products.
+     * This makes the problem easy since we already have a dot product finder.
+     */
+    MatrixMultiplier::transpose_matrix(b);
+
+    // Create plain text objects. Later, the array elements are added to these.
+    helib::Ptxt<helib::BGV> ptxt_a[NCOLS];
+    helib::Ptxt<helib::BGV> ptxt_b[NCOLS];
+    initialize_plaintexts(ptxt_a, NCOLS, encryptor);
+    initialize_plaintexts(ptxt_b, NCOLS, encryptor);
+
+    // Create ciphertext objects. The plaintext objects are encrypted by the public key in the ciphertexts.
+    helib::Ctxt ctxt_a[3]{helib::Ctxt(*(encryptor.getPublicKey())), helib::Ctxt(*(encryptor.getPublicKey())),
+                          helib::Ctxt(*(encryptor.getPublicKey()))};
+    helib::Ctxt ctxt_b[3]{helib::Ctxt(*(encryptor.getPublicKey())), helib::Ctxt(*(encryptor.getPublicKey())),
+                          helib::Ctxt(*(encryptor.getPublicKey()))};
+
+    for (int i = 0; i < NCOLS; ++i) {
+        for (int j = 0; j < NROWS; ++j) {
+            ptxt_a[i][j+1] = a[i][j];
+            ptxt_b[i][j+1] = b[i][j];
+        }
+    }
+
+    for (int i = 0; i < NCOLS; ++i) {
+        encryptor.getPublicKey()->Encrypt(ctxt_a[i], ptxt_a[i]);
+        encryptor.getPublicKey()->Encrypt(ctxt_b[i], ptxt_b[i]);
+    }
+
+    int result[NCOLS][NROWS];
+    for (int i = 0; i < NCOLS; ++i) {
+        for (int j = 0; j < NROWS; ++j) {
+            /*
+             * Now, result[i][j]=dot_product(ctxt_a[i],ctxt_b[j]). However, we need to create a copy of ctxt_a first
+             * as homomorphic dot product stores the result in the first vector (ctxt_a) and we are going to need
+             * the vector again and again.
+             */
+            helib::Ctxt ctxt_a_copy(ctxt_a[i]);
+            DotProduct::dot_product(&ctxt_a_copy, ctxt_b[j], encryptor);
+
+            //Now, all elements of ctxt_a_copy are basically result[i][j]. Select any one of them (by decrypting it).
+            std::vector<long> plaintext(encryptor.getEncryptedArray()->size());
+            encryptor.getEncryptedArray()->decrypt(ctxt_a_copy, *encryptor.getSecretKey(), plaintext);
+            result[i][j] = plaintext[1];
+        }
+    }
+
+    COED::Util::info("Result:");
+    display_matrix(result);
+}
+
+void
+MatrixMultiplier::initialize_plaintexts(helib::Ptxt<helib::BGV> ptxt[], int count, const COED::Encryptor &encryptor) {
+    for (int i = 0; i < count; ++i) {
+        ptxt[i] = helib::Ptxt<helib::BGV>(*(encryptor.getContext()));
+    }
 }
 
 void MatrixMultiplier::accept_inputs(int a[][NROWS], int b[][NROWS]) {
@@ -14,11 +95,11 @@ void MatrixMultiplier::accept_inputs(int a[][NROWS], int b[][NROWS]) {
     int columns = NCOLS;
     int rows = NROWS;
     int total_values = columns * rows;
-    std::cout << "\nEnter <<" << total_values << " values (Hit <Return> after each value)";
+    std::cout << "\nEnter " << total_values << " values (Hit <Return> after each value)" << std::endl;
     MatrixMultiplier::accept_matrix(a);
 
-    std::cout << "\nMatrix 1:";
-    std::cout << "\nEnter <<" << total_values << " values (Hit <Return> after each value)";
+    std::cout << "\nMatrix 2:";
+    std::cout << "\nEnter " << total_values << " values (Hit <Return> after each value)" << std::endl;
     MatrixMultiplier::accept_matrix(b);
 }
 
@@ -32,8 +113,20 @@ void MatrixMultiplier::accept_matrix(int a[][NCOLS]) {
 
 void MatrixMultiplier::transpose_matrix(int a[][NCOLS]) {
     for (int i = 0; i < NCOLS; ++i) {
-        for (int j = 0; j < NROWS; ++j) {
+        for (int j = 0; j <= i; ++j) {
+            int temp = a[i][j];
             a[i][j] = a[j][i];
+            a[j][i] = temp;
         }
     }
 }
+
+void MatrixMultiplier::display_matrix(int a[][NCOLS]) {
+    for (int i = 0; i < NCOLS; ++i) {
+        for (int j = 0; j < NROWS; ++j) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
--- source/MatrixMultiplier.h
+++ source/MatrixMultiplier.h
@@ -7,15 +7,24 @@
 #define NROWS 3
 #define NCOLS 3
 
+#include "Encryptor.h"
+
 class MatrixMultiplier {
 public:
     static void main();
 
 private:
     static void accept_inputs(int a[][NCOLS], int b[][NROWS]);
+
     static void transpose_matrix(int a[][NCOLS]);
+
     static void accept_matrix(int a[][NCOLS]);
 
+    static void display_matrix(int a[][NCOLS]);
+
+    static void initialize_plaintexts(helib::Ptxt<helib::BGV> ptxt[], int count, const COED::Encryptor &encryptor);
+
+    static void initialize_ciphertexts(helib::Ctxt ctxt[], int count, const COED::Encryptor &encryptor);
 };
 
 
