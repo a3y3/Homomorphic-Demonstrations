ae9b216ac2666770e3df37da22b279cd72297f95
a3y3 <sohamssd@gmail.com>
2020-10-14 10:32:15 -0400

    Refactor 2D arrays to use double pointers

--- source/ConvolutionFilterEvaluator.cpp
+++ source/ConvolutionFilterEvaluator.cpp
@@ -7,30 +7,39 @@
 #include "Encryptor.h"
 #include "Util.h"
 
-#define INPUT_DATA_COLUMNS 6
-#define INPUT_DATA_ROWS 6
-#define FILTER_ROWS 3
-#define FILTER_COLUMNS 3
-#define FEATURE_MAP_COLUMNS 4
-#define FEATURE_MAP_ROWS 4
 
 void ConvolutionFilterEvaluator::main() {
+    int **input_data;
+    input_data = new int *[INPUT_DATA_COLUMNS];
+    for (int i = 0; i < INPUT_DATA_COLUMNS; ++i) {
+        input_data[i] = new int[INPUT_DATA_ROWS];
+    }
+    int **filter;
+    filter=new int *[FILTER_COLUMNS];
+    for (int i = 0; i < FILTER_COLUMNS; ++i) {
+        filter[i]=new int[FILTER_ROWS];
+    }
+    ConvolutionFilterEvaluator::accept_inputs(input_data, filter);
+    COED::Util::info("Input data:");
+    display_matrix(input_data, INPUT_DATA_COLUMNS, INPUT_DATA_ROWS);
+    COED::Util::info("Filter:");
+    display_matrix(filter, FILTER_COLUMNS, FILTER_ROWS);
 
 }
 
-void accept_inputs(int input_data[][INPUT_DATA_ROWS], int filter[][FILTER_ROWS]) {
+void ConvolutionFilterEvaluator::accept_inputs(int **input_data, int **filter) {
     std::cout << "\nInput data is a 6x6 matrix.";
     for (int i = 0; i < INPUT_DATA_COLUMNS; ++i) {
-        std::cout << "\nInput " << INPUT_DATA_COLUMNS << " values for input data's row number" << i
+        std::cout << "\nInput " << INPUT_DATA_COLUMNS << " values for input data's row number " << i
                   << " (Hit <Return> after each)";
         for (int j = 0; j < INPUT_DATA_COLUMNS; ++j) {
             std::cin >> input_data[i][j];
         }
     }
 
-    std::cout << "\n The filter is a 3x3 matrix.";
+    std::cout << "\nThe filter is a 3x3 matrix.";
     for (int i = 0; i < FILTER_COLUMNS; ++i) {
-        std::cout << "\nInput " << FILTER_COLUMNS << " values for input data's row number" << i
+        std::cout << "\nInput " << FILTER_COLUMNS << " values for input data's row number " << i
                   << " (Hit <Return> after each)";
         for (int j = 0; j < FILTER_ROWS; ++j) {
             std::cin >> filter[i][j];
@@ -38,7 +47,7 @@ void accept_inputs(int input_data[][INPUT_DATA_ROWS], int filter[][FILTER_ROWS])
     }
 }
 
-void ConvolutionFilterEvaluator::display_matrix(int a[6][6], int m, int n) {
+void ConvolutionFilterEvaluator::display_matrix(int **a, int m, int n) {
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
             std::cout << a[i][j] << " ";
--- source/ConvolutionFilterEvaluator.h
+++ source/ConvolutionFilterEvaluator.h
@@ -4,6 +4,12 @@
 
 #ifndef HOMOMORPHIC_DEMONSTRATIONS_CONVOLUTIONFILTEREVALUATOR_H
 #define HOMOMORPHIC_DEMONSTRATIONS_CONVOLUTIONFILTEREVALUATOR_H
+#define INPUT_DATA_COLUMNS 6
+#define INPUT_DATA_ROWS 6
+#define FILTER_ROWS 3
+#define FILTER_COLUMNS 3
+#define FEATURE_MAP_COLUMNS 4
+#define FEATURE_MAP_ROWS 4
 
 
 class ConvolutionFilterEvaluator {
@@ -11,7 +17,8 @@ public:
     static void main();
 
 private:
-    static void display_matrix(int a[6][6], int m, int n);
+    static void display_matrix(int **a, int m, int n);
+    static void accept_inputs(int **input_data, int **filter);
 };
 
 
